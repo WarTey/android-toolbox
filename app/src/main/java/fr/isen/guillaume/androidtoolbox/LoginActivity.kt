f0c9ad5284afbae9d6edc92183482d5b5cf5367e
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-02-08 14:24:03 +0100

    Amélioration de l'interface LoginActivity

--- app/src/main/java/fr/isen/guillaume/androidtoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/guillaume/androidtoolbox/LoginActivity.kt
@@ -5,7 +5,10 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import android.widget.Toast
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -14,20 +17,30 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        initLayout()
+
         val sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         if (sharedPreferences.getString("username", "").equals(USERNAME) && sharedPreferences.getString("password", "").equals(PASSWORD))
             goToHome()
 
         btnSend.setOnClickListener { login(sharedPreferences) }
+        btnForget.setOnClickListener { viewLoginInfos() }
+    }
+
+    private fun initLayout() {
+        Handler().postDelayed({
+            constraintLayoutInput.visibility = View.VISIBLE
+            constraintLayoutConnection.visibility = View.VISIBLE
+        }, 2500)
     }
 
     private fun login(sharedPreferences: SharedPreferences) {
-        if (authCheck()) {
+        if (inputCheck()) {
             setSharedPreferences(sharedPreferences)
             Toast.makeText(this, getString(R.string.welcome_name, txtUsername.text.toString()), Toast.LENGTH_LONG).show()
             goToHome()
         } else
-            Toast.makeText(this, getString(R.string.wrong_credentials), Toast.LENGTH_LONG).show()
+            viewBadInput()
     }
 
     private fun setSharedPreferences(sharedPreferences: SharedPreferences) {
@@ -37,8 +50,20 @@ class LoginActivity : AppCompatActivity() {
         editor.apply()
     }
 
-    private fun authCheck(): Boolean {
+    private fun inputCheck(): Boolean {
         return txtUsername.text.toString() == USERNAME && txtPassword.text.toString() == PASSWORD
+    }
+
+    private fun viewBadInput() {
+        if (txtUsername.text.toString() != USERNAME)
+            inputUsername.error = "Identifiant incorrect"
+        else
+            inputUsername.error = null
+
+        if (txtPassword.text.toString() != PASSWORD)
+            inputPassword.error = "Mot de passe incorrect"
+        else
+            inputPassword.error = null
     }
 
     private fun goToHome() {
@@ -46,6 +71,10 @@ class LoginActivity : AppCompatActivity() {
         intentHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intentHome)
         finish()
+    }
+
+    private fun viewLoginInfos() {
+        MaterialAlertDialogBuilder(this).setTitle(getString(R.string.password_forget)).setMessage(getString(R.string.password_retrieve, USERNAME, PASSWORD)).setPositiveButton(getString(R.string.ok_btn), null).show()
     }
 
     companion object {
